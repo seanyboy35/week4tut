ce37c480bb1cf49bd24db7ffa93453035d8bd4f0
AnmolB18 <molanmol11@gmail.com>
2024-08-08 09:09:15 +1000

    Week 4 Tutorial setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AccountComponent } from './account/account.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'account', component: AccountComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }  // Default route
+];
